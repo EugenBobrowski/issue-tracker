f3bd3e2366a9b57339f5fda02810caf7be674fe5
Eugen Bobrowski <ecologistkai@gmail.com>
2016-10-16 22:42:35 +0300

    Create issues table

--- shortcode-dashboard.php
+++ shortcode-dashboard.php
@@ -1,8 +1,8 @@
 <?php
 
 
-
-class Issue_Tracker_Shortcode_Dashboard {
+class Issue_Tracker_Shortcode_Dashboard
+{
 
     protected static $instance;
 
@@ -11,37 +11,51 @@ class Issue_Tracker_Shortcode_Dashboard {
         add_shortcode('issues_dashboard', array($this, 'shortcode'));
     }
 
-    public function shortcode ($attr) {
+    public function shortcode($attr)
+    {
 
         wp_enqueue_style('issues-front', plugin_dir_url(__FILE__) . 'assets/issues-front.css');
 
-        $issuers_query = new WP_Query( array('post_type'=> 'issues') );
+        $issuers_query = new WP_Query(array('post_type' => 'issues'));
 
         ob_start();
 
         ?>
         <div class="issues-dashboard">
-        <?php
-
-        if ( $issuers_query->have_posts() ) { ?>
-            <ul class="issues-list"> <?php
-            while ( $issuers_query->have_posts() ) {
-                $issuers_query->the_post();
-
-                ?>
-                <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-                <?php
-            }
-            ?>
-            </ul>
             <?php
-            /* Restore original Post Data */
-            wp_reset_postdata();
-        } else {
-            // no posts found
-        }
 
-        ?>
+            if ($issuers_query->have_posts()) { ?>
+                <table class="issues-list">
+                    <thead>
+                    <tr>
+                        <td>#</td>
+                        <th><?php _e('Issue', 'issue-tracker'); ?></th>
+                        <th><?php _e('Priority', 'issue-tracker'); ?></th>
+                        <th><?php _e('Project', 'issue-tracker'); ?></th>
+                    </tr>
+                    </thead><?php
+                    while ($issuers_query->have_posts()) {
+                        $issuers_query->the_post();
+
+                        ?>
+                        <tr>
+                            <th><?php echo get_the_ID(); ?></th>
+                            <td><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
+                </table>
+                <?php
+                /* Restore original Post Data */
+                wp_reset_postdata();
+            } else {
+                // no posts found
+            }
+
+            ?>
         </div>
         <?php
 
@@ -49,8 +63,8 @@ class Issue_Tracker_Shortcode_Dashboard {
     }
 
 
-
-    public function check_required_plugins () {
+    public function check_required_plugins()
+    {
         return in_array('fields-for-all/fields-for-all.php', apply_filters('active_plugins', get_option('active_plugins')));
     }
 
